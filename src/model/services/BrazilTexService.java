dde6345aca882dc26f4ca7efcd3ec583b2b16ec9
Gabriel Genaro <g.genaro.contato@gmail.com>
2024-08-02 18:54:48 -0300

    Projeto Locadora com a ultilização de interface

--- src/model/services/BrazilTexService.java
+++ src/model/services/BrazilTexService.java
@@ -1,8 +1,8 @@
 package model.services;
 
-public class BrazilTexService {
+public class BrazilTexService implements TaxService{
 	
-	public Double tax(double amount) {
+	public double tax(double amount) {
 		if(amount <= 100.0) {
 			return amount * 0.2;
 		}
