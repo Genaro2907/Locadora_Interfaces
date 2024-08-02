55ecc8958b02aa9d2c42df0dfd44be6f871d4632
Gabriel Genaro <g.genaro.contato@gmail.com>
2024-08-02 18:44:48 -0300

    Finalizando Projeto Locadora sem Interface

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,6 +7,8 @@ import java.util.Scanner;
 
 import model.entities.CarRental;
 import model.entities.Vehicle;
+import model.services.BrazilTexService;
+import model.services.RentalService;
 
 public class Program {
 
@@ -14,17 +16,30 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
-		System.out.print("Entre com os dados do aluguel");
+		System.out.println("Entre com os dados do aluguel");
 		System.out.print("Modelo do carro: ");
 		String carModel = sc.nextLine();
-		System.out.println("Retirada (dd/MM/yyyy HH:mm): ");
+		System.out.print("Retirada (dd/MM/yyyy HH:mm): ");
 		LocalDateTime start = LocalDateTime.parse(sc.nextLine(), fmt);
-		System.out.println("Retorno (dd/MM/yyyy HH:mm): ");
+		System.out.print("Retorno (dd/MM/yyyy HH:mm): ");
 		LocalDateTime finish = LocalDateTime.parse(sc.nextLine(), fmt);
 
 		CarRental cr = new CarRental(start, finish, new Vehicle(carModel));
 
-		sc.close(); 
+		System.out.print("Entre com o preço por hora: ");
+		double pricePerHour = sc.nextDouble();
+		System.out.print("Entre com o preço por dia: ");
+		double pricePerDay = sc.nextDouble();
+
+		RentalService rentalService = new RentalService(pricePerHour, pricePerDay, new BrazilTexService());
+		rentalService.processInvoice(cr);
+		
+		System.out.println("FATURA: ");
+		System.out.println("Pagamento básico: " + String.format("%.2f", cr.getInvoice().getBasicPayment()));
+		System.out.println("Imposto: " + cr.getInvoice().getTax() );
+		System.out.println("Pagamento total: " + String.format("%.2f", cr.getInvoice().getBasicPayment()));
+
+		sc.close();
 
 	}
 
